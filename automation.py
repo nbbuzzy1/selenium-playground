602c9e08f33bf5f04fde1a0ce47f7614044ce635
nbbuzzy1 <40919504+nbbuzzy1@users.noreply.github.com>
2020-06-24 06:39:07 -0400

    Add browser interaction.

--- automation.py
+++ automation.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+import time
 
 chrome_browser = webdriver.Chrome('./chromedriver.exe')
 
@@ -6,7 +7,17 @@ chrome_browser.maximize_window()
 chrome_browser.get(
     'https://www.seleniumeasy.com/test/basic-first-form-demo.html')
 
-print('Selenium Easy Demo' in chrome_browser.title)
 
-button_text = chrome_browser.find_element_by_class_name('btn-default')
-print(button_text.get_attribute('innerHTML'))
+show_msg_button = chrome_browser.find_element_by_class_name('btn-default')
+assert 'Show Message' in chrome_browser.page_source
+
+user_message = chrome_browser.find_element_by_id('user-message')
+user_message.clear()
+user_message.send_keys('I am extra cool')
+
+# new_el = chrome_browser.find_element_by_css_selector('.btn')
+
+# show_msg_button.click()
+
+chrome_browser.close()
+chrome_browser.close()  # or quit()
